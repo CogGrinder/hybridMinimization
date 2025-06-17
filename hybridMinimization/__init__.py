7347b3958a79d262802bcb3f91198a3323a42222
vmeduski <vincent.meduski@etu.univ-grenoble-alpes.fr>
2025-06-17 15:33:32 +0200

    trying to flat all base files apart from driver.py
    Note: we assume that driver.py should not be used as a library but as an example in most contexts.

--- hybridMinimization/__init__.py
+++ hybridMinimization/__init__.py
@@ -1,6 +1,16 @@
 from hybridMinimization.__version__ import __version__
-from hybridMinimization.hybridMinimization import (
-    hybridMinimization,
-)
+from hybridMinimization.hybridMinimization import *
+from hybridMinimization.kernelClass import *
+from hybridMinimization.syntheticFunctions import *
+from hybridMinimization.universalConfigConversion import *
+
 name = 'hybridMinimization'
 version = __version__
+
+__all__ = [
+    'hybridMinimization',
+    'kernelClass',
+    'syntheticFunctions',
+    'universalConfigConversion',
+    '__version__',
+]
